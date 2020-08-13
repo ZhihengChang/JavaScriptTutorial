0d305b5d6ac53dd0cd2d71409a5aa9cb35a5629f
ZhihengChang <zhihengchang602@gmail.com>
2020-08-13 11:35:39 -0400

    add useStrict.js to Part1 Fundamentals

---
+++ Part1/Fundamentals/useStrict.js
@@ -0,0 +1,7 @@
+"use strict";
+/*
+Modern JavaScript supports “classes” and “modules” 
+– advanced language structures (we’ll surely get to them), that enable use strict automatically. 
+So we don’t need to add the "use strict" directive, if we use them.
+*/
+console.log("This script use strict");
