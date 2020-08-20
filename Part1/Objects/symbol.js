3e6a697da6af4b304fac43d5be32c78fa064bde2
ZhihengChang <zhihengchang602@gmail.com>
2020-08-20 12:33:31 -0400

    objects: finish symbol.js

--- Part1/Objects/symbol.js
+++ Part1/Objects/symbol.js
@@ -25,8 +25,36 @@ console.log(id.description);
 Symbols allow us to create “hidden” properties of an object, 
 that no other part of code can accidentally access or overwrite.
 */
-let user = { name: "Ryan" };
+let user = { name: "Jhon" };
 let userId = Symbol("id");
 user[userId] = 1;
 console.log(user[userId]);
 // console.log(user["userId"]); //Error
+
+//Symbols in an object literal
+let user2 = {
+    name: "Ryan",
+    [id]: 123 //not "id": 123
+}
+
+//Symbolic properties do not participate in for in loop
+user2.age = 21; //name, age, symbol id
+for(let key in user2) console.log(key); //name age
+console.log(Object.keys(user2));
+//the direct access by the symbol works
+console.log(user2[id]);
+
+//however object.assign copies both string and symbol properties:
+let clone = Object.assign({},user2);
+console.log(clone[id]);
+
+//Global symbols
+let gobalId = Symbol.for("id");
+// if the symbol did not exist, it is created
+let idAgain = Symbol.for("id");
+console.log(gobalId === idAgain);
+
+//Symbol.keyFor(sym) returns a name by a global symbol
+console.log(Symbol.keyFor(gobalId));
+//it doesn't work for non-global symbols:
+console.log(Symbol.keyFor(userId));
