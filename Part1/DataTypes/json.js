4ea77082ff06c3bdb584c1cc30c96e82cc2b6efd
ZhihengChang <zhihengchang602@gmail.com>
2020-09-02 13:15:57 -0400

    dataTypes: add json.js

--- Part1/DataTypes/json.js
+++ Part1/DataTypes/json.js
@@ -47,3 +47,44 @@ console.log(JSON.stringify(shoppingCart, function replacer(key, value){
     return (key == 'items' || key == 'oneDayShipping')?
     undefined : value; 
 }));
+
+//formatting:
+console.log(JSON.stringify(shoppingCart, null, 2));
+
+//custom "toJSON"
+let room = {
+    number: 23,
+    //default toJSON returns: {number: 23}
+    toJSON() {
+      return this.number;
+    }
+};
+let meetup = {
+    title: "Conference",
+    room
+};
+console.log( JSON.stringify(room) ); // 23
+console.log( JSON.stringify(meetup) );
+
+//JSON.parse: decode json string
+let str = `
+{"name": "John",
+"age": 21,
+"isAdmin": false,
+"friends": [1,2,3],
+"parents": {
+"mom": "Mary",
+"dad": "Tom"
+}}`;
+let person = JSON.parse(str);
+console.log(person);
+
+//reviver:
+str = '{"title":"Conference", "date":"2020-11-30T12:00:00.000Z"}';
+// let meeting = JSON.parse(str);
+// console.log(meeting); //the date is a string not a date object
+let meeting = JSON.parse(str, function(key, value) {
+    if (key == 'date') return new Date(value);
+    return value;
+  });
+console.log(meeting);
