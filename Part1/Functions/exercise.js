df47e6bb792c1646ff3b022bc91246dd865d121d
ZhihengChang <zhihengchang602@gmail.com>
2020-09-05 16:31:07 -0400

    functions: add more to exercise.js

--- Part1/Functions/exercise.js
+++ Part1/Functions/exercise.js
@@ -39,4 +39,36 @@ let arr = [a, b, c, d, e];
 // compare.key = 'label';
 // compare.order = 'asc';
 // console.log(arr.sort(compare));
-console.log(sort_by(arr, 'label', 'asc'));
+// console.log(sort_by(arr, 'label', 'asc')); 
+
+function work(a, b){
+    console.log(a + b);
+}
+function spy(func){
+    function wrapper(...args){
+        wrapper.calls.push(args);
+        return func.apply(this, args);
+    }
+    wrapper.calls = [];
+    return wrapper;
+}
+work = spy(work);
+work(1, 2); 
+work(4, 5); 
+for (let args of work.calls) {
+  console.log( 'call:' + args.join() );
+}
+
+function f(x) {
+    console.log(x);
+}
+function delay(f, ms){
+    return function(){
+        setTimeout(()=> f.apply(this, arguments), ms);
+    };
+}
+let f1000 = delay(f, 1000);
+let f1500 = delay(f, 1500);
+  
+f1000("test1");
+f1500("test2");
