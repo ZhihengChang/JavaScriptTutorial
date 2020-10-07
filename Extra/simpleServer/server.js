91f12872bcbf346f41afb350b75f9ec057bdad09
ZhihengChang <zhihengchang602@gmail.com>
2020-10-07 15:49:49 -0400

    In Progress: server.js and main.html

--- Extra/simpleServer/server.js
+++ Extra/simpleServer/server.js
@@ -1,6 +1,22 @@
 var http = require('http');
+var fs = require('fs');
+var formidable = require('formidable');
+
+
 http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello World');
-    res.end();
-}).listen(3001);
+    console.log('connected to server')
+    if(req.url == '/uploadform'){
+        res.writeHead(200);
+        res.write(fs.readFileSync('main.html'));
+        return res.end();
+    }else if(req.url == '/fileupload'){
+        console.log("waiting for file");
+        res.writeHead(200);
+        res.write("Recieved");
+        let form = new formidable.IncomingForm();
+        form.parse(req).on('file', function(name, file){
+            console.log('got file: ', name);
+        })
+        return res.end();
+    }
+}).listen(3002);
