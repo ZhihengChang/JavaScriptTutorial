52de0cb15c86707caf1acb18e4603b73ecc5c6b9
ZhihengChang <zhihengchang602@gmail.com>
2020-10-09 15:41:45 -0400

    uncomment processPost()

--- Extra/simpleServer/server.js
+++ Extra/simpleServer/server.js
@@ -1,23 +1,23 @@
 var http = require('http');
 var fs = require('fs');
-var formidable = require('../node_modules/formidable');
+//var formidable = require('../node_modules/formidable');
 var qs = require('querystring');
 
 
 http.createServer(function(req, res){
     console.log('connected to server')
     console.log(req.headers);
-    let form = new formidable.IncomingForm();
+    //let form = new formidable.IncomingForm();
     if(req.url == '/uploadform'){
         res.writeHead(200);
         res.write(fs.readFileSync('main.html'));
         return res.end();
     }else if(req.url == '/fileupload'){
-        // processPost(req, res, function(){
-        //     console.log(req.post);
-        //     res.writeHead(200, "OK", {'Content-Type': 'text/plain'});
-        //     res.end();
-        // });
+        processPost(req, res, function(){
+            console.log(req.post);
+            res.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+            res.end();
+        });
         //handle base64String
     }else{
         res.writeHead(200, "OK", {'Content-Type': 'text/plain'});
@@ -25,19 +25,20 @@ http.createServer(function(req, res){
     }
 }).listen(3002);
 
-// function processPost(req, res, callback){
-//     var queryData = "";
-//     if(req.method == 'POST'){
-//         req.on('data', function(data){
-//             queryData += data;
-//         });
-//         req.on('end', function(){
-//             console.log(queryData);
-//             req.post = qs.parse(queryData);
-//             callback();
-//         });
-//     }else{
-//         res.writeHead(405, {'Content-Type': 'text/plain'});
-//         res.end();
-//     }
-// }
+function processPost(req, res, callback){
+    var queryData = "";
+    if(req.method == 'POST'){
+        req.on('data', function(data){
+            queryData += data;
+        });
+        req.on('end', function(){
+            console.log(queryData);
+            console.log("-------------------------------------------");
+            req.post = qs.parse(queryData);
+            callback();
+        });
+    }else{
+        res.writeHead(405, {'Content-Type': 'text/plain'});
+        res.end();
+    }
+}
